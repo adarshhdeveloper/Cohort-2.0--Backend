6e130ee81dacb7ebf316de0ebc59eb088b766b18
Adarsh Prajapati <code.adarsh121@gmail.com>
2026-01-27 20:18:19 +0530

    used puth method

--- Day5/server.js
+++ Day5/server.js
@@ -3,5 +3,5 @@
 const app = require('./src/app')
 
 app.listen(3000,()=>{
-    console.log("erver is running on port 3000")
+    console.log(`erver is running on port 3000 cleck here http://localhost:3000/`)
 })
--- Day5/src/app.js
+++ Day5/src/app.js
@@ -6,7 +6,25 @@ const express =require('express')
 let notes =[]
 
 const app = express() //server intance 
-app.post('/notes',(req,res)={
+ 
+app.use(express.json()) 
+
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+    res.send("Notes Created")
+   
+
+})
+app.patch("/notes",(req,res)=>{
+    notes.push(req.body)
+    res.send("Notes Updated")
+})
+app.put("/note",(req,res)=>{
+    
+})
+app.get("/notes",(req,res)=>{
+    res.send("Notes Recived")
+    console.log(notes)
     
 })
 
