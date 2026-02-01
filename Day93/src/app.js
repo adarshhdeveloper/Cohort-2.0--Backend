0cd581dee373422112da6e8e62700a1ae07efd35
Adarsh Prajapati <code.adarsh121@gmail.com>
2026-02-01 20:53:33 +0530

    Day93

---
+++ Day93/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Server is running...'
+    })
+})
+
+module.exports = app
